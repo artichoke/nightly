7aed6d1376f0543fa386e238205dcd2647efaabf
Ryan Lopopolo <rjl@hyperbo.la>
2025-03-05 18:17:24 -0800

    simplify extras logger

--- artichoke_nightly/_utils/logger.py
+++ artichoke_nightly/_utils/logger.py
@@ -1,47 +1,39 @@
 import logging
 import sys
+from functools import cache
 
 
-class CustomFormatter(logging.Formatter):
+@cache
+def standard_log_record_attrs() -> set[str]:
     """
-    Custom formatter that appends extra keys from the log record formatted as
-    `key=value another.key=a_thing` to the log message.
-    """
+    Get the standard attributes of a LogRecord.
 
+    This function is cached to avoid recalculating the set of standard attributes
+    for each LogRecord instance.
+
+    Returns:
+        set[str]: The standard attributes of a LogRecord.
+    """
+    return set(
+        logging.LogRecord("", logging.INFO, "", 1, "", None, None, "").__dict__.keys()
+    )
+
+
+class ExtrasKeyValueFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
-        # First, format the message normally.
-        formatted_message = super().format(record)
-        # Define standard LogRecord attributes to exclude from extras.
-        standard_attrs = {
-            "name",
-            "msg",
-            "args",
-            "levelname",
-            "levelno",
-            "pathname",
-            "filename",
-            "module",
-            "exc_info",
-            "exc_text",
-            "stack_info",
-            "lineno",
-            "funcName",
-            "created",
-            "msecs",
-            "relativeCreated",
-            "thread",
-            "threadName",
-            "processName",
-            "process",
-            "message",
+        base_log = super().format(record)
+
+        # Extract only extra fields
+        extra_fields = {
+            k: v
+            for k, v in record.__dict__.items()
+            if k not in standard_log_record_attrs()
         }
-        # Collect extra attributes that were passed in.
-        extras = {k: v for k, v in record.__dict__.items() if k not in standard_attrs}
-        if extras:
-            # Format extras as key=value pairs separated by a space.
-            extras_str = " ".join(f"{k}={v}" for k, v in extras.items())
-            formatted_message = f"{formatted_message} | {extras_str}"
-        return formatted_message
+
+        # Format extra fields as key=value
+        extra_str = " ".join(f"{k}={v}" for k, v in extra_fields.items())
+
+        return f"{base_log} {extra_str}".strip()
 
 
 def setup_logger() -> None:
@@ -59,7 +51,9 @@ def setup_logger() -> None:
 
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setLevel(logging.INFO)
-    formatter = CustomFormatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = ExtrasKeyValueFormatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
 
